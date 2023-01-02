8ab926246639c720306ce7b715027a6401ed70e7
Sergio Andrés Herrera Velásquez <82914044+sahvsergio@users.noreply.github.com>
2023-01-01 21:20:20 -0500

    Update settings.py

--- premioplatziapp/premioplatziapp/settings.py
+++ premioplatziapp/premioplatziapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!y!za@^*zf73a3lg0-9qgrrwaj#5@k2lm*$g*v6ao2d!0(1--&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = []
 
